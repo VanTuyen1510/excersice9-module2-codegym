ddc22fc56b6ee61ece03728089277ae3697c6d49
VanTuyen1510 <tisiris1997@gmail.com>
2023-03-03 17:48:20 +0700

    thêm file java

--- exercise_three/checkThePalindromeString.java
+++ exercise_three/checkThePalindromeString.java
@@ -7,9 +7,9 @@ import java.util.Stack;
 
 class CheckPalindrome {
     public static void main(String[] args) {
-        String str = "Able was I ere I saw Alba";
+        String str = "Able was I ere I was Able";
         String  [] myArr = str.split(" ");
-
+  // "" là tách từng ký tự ,  còn " " tách từng từ
         Stack<String> stackStr = new Stack<>();
         Queue<String> queueStr = new LinkedList<>();
         for (int i = 0; i < myArr.length; i++) {
@@ -17,14 +17,14 @@ class CheckPalindrome {
             queueStr.offer(myArr[i]);
         }
 
-        boolean check = false;
+        boolean check = false; //Đặt cờ ko quan trọng đặt true false
         while (!stackStr.isEmpty()) {
             if (!stackStr.pop().equals(queueStr.remove())) {
                 check = true;
                 break;
             }
         }
-        if(check){
+        if(!check){
             System.out.println("This is Palindrome");
         }else {
             System.out.println("This is not Palindrome");
